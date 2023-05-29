1902cf408c3b3881e16c657b150c26255fbbc0a1
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-05-29 12:02:41 +0100

    update on the viewmodel and model

--- Networking/Networking/Controllers/HomeViewController.swift
+++ Networking/Networking/Controllers/HomeViewController.swift
@@ -19,7 +19,7 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
     
---
+++ Networking/Networking/Model/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  Networking
+//
+//  Created by Mac on 29/05/2023.
+//
+
+import Foundation
