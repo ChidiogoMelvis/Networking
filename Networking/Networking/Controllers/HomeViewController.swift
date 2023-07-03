e6529478444887c27fa13260bfab80d7f35ea751
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-07-03 23:38:41 +0100

    updated

--- Networking/Networking/Controllers/HomeViewController.swift
+++ Networking/Networking/Controllers/HomeViewController.swift
@@ -7,9 +7,18 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
     
     let viewModel = UserViewModel()
+    
+    lazy var searchTextField: UISearchTextField = {
+        let search = UISearchTextField()
+        search.translatesAutoresizingMaskIntoConstraints = false
+        search.delegate = self
+        search.addTarget(self, action: #selector(searchTextChanged), for: .editingChanged)
+        return search
+        
+    }()
     
     lazy var tableView: UITableView = {
         let tableView = UITableView()
@@ -21,6 +30,7 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
         view.backgroundColor = .white
         viewModel.getUsers { [weak self] error in
                     if let error = error {
@@ -33,6 +43,15 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
                 }
             }
     
+    @objc func searchTextChanged() {
+        // Access the current text in the search text field
+        let searchText = searchTextField.text ?? ""
+        
+        // Perform any desired actions based on the search text
+        print("Search text changed: \(searchText)")
+    }
+
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.users.count
     }
@@ -40,6 +59,22 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell", for: indexPath)
         return cell
+    }
+    
+    func setupViews() {
+        view.addSubview(searchTextField)
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+        searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+        searchTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+        searchTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+        
+        tableView.topAnchor.constraint(equalTo: searchTextField.bottomAnchor, constant: 16),
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10)
+        ])
     }
     
     
